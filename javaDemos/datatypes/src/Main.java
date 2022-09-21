408815550a856385cee60c938b48bba0f76c9387
ConstantineTr <114101642+ConstantineTr@users.noreply.github.com>
2022-09-21 20:51:21 +0300

    Add files via upload

---
+++ javaDemos/datatypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi =12;
+        sayi = 1311111111;
+        byte sayi1 =12;
+        sayi1 = 127;
+        double sayi2 =12.5;
+        char karakter = 'A';
+        boolean dogrumMu=false;
+
+    }
+}
